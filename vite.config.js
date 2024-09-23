0d80e65e6ce325e36e1c4e7381c1154cca7cc50c
deepak-bulani <deepak.bulani@contentstack.com>
2024-09-23 18:21:55 +0530

    feat: host mf implementation

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    target: 'esnext',
+    modulePreload: false
+  },
+  plugins: [
+    react(),
+    federation({
+      name: "app",
+      filename: "remoteEntry.js",
+      remotes: {
+        cms: "http://localhost:3001/assets/cmsEntry.js",
+      },
+      shared: ["react", "react-dom"]
+    })
+  ],
 })
